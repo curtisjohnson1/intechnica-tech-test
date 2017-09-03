4dbdc537d666e7ac68b38e54d968bcd39ff8f146
curtis johnson <curtisianjohnson@gmail.com>
2017-09-03 11:08:55 +0100

    create test env - first test complete

---
+++ spec/trafficDefender.spec.js
@@ -0,0 +1,18 @@
+const mocha = require('mocha');
+const coMocha = require('co-mocha');
+const expect = require('chai').expect;
+const test = require('selenium-webdriver/testing');
+const { Builder, By, until } = require('selenium-webdriver');
+coMocha(mocha);
+
+const driver = new Builder().forBrowser('chrome').build();
+const testPage = require('../trafficDefenderPageObj')(driver);
+
+test.describe('trafficDefender', function() {
+  before(() => testPage.navigateToTestPage());
+  after(() => driver.quit());
+
+  test.it('navigates to test page', function*() {
+    expect(yield testPage.checkPageTitle()).to.equal('Perf test server 1');
+  });
+})
