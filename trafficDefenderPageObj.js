7fe036bca6711986d3662628c7cd982241a51325
curtis johnson <curtisianjohnson@gmail.com>
2017-09-03 11:07:31 +0100

    added selenium commands to get url and check page title

---
+++ trafficDefenderPageObj.js
@@ -0,0 +1,20 @@
+const { Builder, By, until } = require('selenium-webdriver');
+
+module.exports = function(driver) {
+  const elements = {
+    queueNumber: By.className('odometer-value')
+  };
+  return {
+    url: 'http://34.252.63.57/',
+    holdingPage: 'TrafficDefender Holding Page',
+    navigateToTestPage: function() {
+      return driver.get(this.url);
+    },
+    checkPageTitle: function() {
+      return driver.getTitle(title => {
+        return title;
+      });
+    }
+  }
+
+}
